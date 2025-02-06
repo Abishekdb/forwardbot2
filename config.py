0fea22589d40e7aa65129785936b3ce0e37b1b5a
Abishekdb <dbtelegrambots@gmail.com>
2025-02-06 18:19:05 +0530

    Update config.py

--- config.py
+++ config.py
@@ -10,10 +10,10 @@ class Config:
     API_HASH = environ.get("API_HASH", "")
     BOT_TOKEN = environ.get("BOT_TOKEN", "") 
     BOT_SESSION = environ.get("BOT_SESSION", "Auto_Forward") 
-    DATABASE_URI = environ.get("DATABASE", "")
-    DATABASE_NAME = environ.get("DATABASE_NAME", "")
-    BOT_OWNER_ID = [int(id) for id in environ.get("BOT_OWNER_ID", '').split()]
-    LOG_CHANNEL = int(environ.get('LOG_CHANNEL', ''))
+    DATABASE_URI = environ.get("DATABASE", "mongodb+srv://dbtelegrambots4:priyanka@cluster0.frwrh.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+    DATABASE_NAME = environ.get("DATABASE_NAME", "Cluster0")
+    BOT_OWNER_ID = [int(id) for id in environ.get("BOT_OWNER_ID", '7562335183').split()]
+    LOG_CHANNEL = int(environ.get('LOG_CHANNEL', '-1002351403140'))
     FORCE_SUB_CHANNEL = environ.get("FORCE_SUB_CHANNEL", "") 
     FORCE_SUB_ON = environ.get("FORCE_SUB_ON", "False")
     PORT = environ.get('PORT', '8080')

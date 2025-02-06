305afbf0d7e3454186e62f3e9acbff4c1eab723d
Abishekdb <dbtelegrambots@gmail.com>
2025-02-06 22:02:43 +0530

    Update config.py

--- config.py
+++ config.py
@@ -6,10 +6,10 @@ from os import environ
 # For Any Kind Of Error Ask Us In Support Group @Silicon_Botz 
 
 class Config:
-    API_ID = environ.get("API_ID", "")
-    API_HASH = environ.get("API_HASH", "")
+    API_ID = environ.get("API_ID", "20842824")
+    API_HASH = environ.get("API_HASH", "8ded77e28e1eb760ca30704bc4a6eeaf")
     BOT_TOKEN = environ.get("BOT_TOKEN", "") 
-    BOT_SESSION = environ.get("BOT_SESSION", "") 
+    BOT_SESSION = environ.get("BOT_SESSION", "testingforward12345bot") 
     DATABASE_URI = environ.get("DATABASE", "mongodb+srv://dbtelegrambots4:priyanka@cluster0.frwrh.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
     DATABASE_NAME = environ.get("DATABASE_NAME", "Cluster0")
     BOT_OWNER_ID = [int(id) for id in environ.get("BOT_OWNER_ID", '7562335183').split()]
